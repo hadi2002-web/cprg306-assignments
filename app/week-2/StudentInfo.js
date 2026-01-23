4cc9dbb69a8820d5abe48003b36eccdcee04fb6d
Firdows Hadi <firdowshadi@Firdowss-MacBook-Air-2.local>
2026-01-23 09:39:29 -0700

    new commit

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: firdowshadi</p>
+      <p>
+        GitHub: <Link href="https://github.com/hadi2002-web/cprg306-assignments">hadi2002-web/cprg306-assignments</Link>
+      </p>
+    </div>
+  );
+}
