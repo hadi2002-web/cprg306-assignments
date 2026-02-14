5b6fcfacb07969994433358ce6a77a5803eab63c
Firdows Hadi <firdowshadi@Mac.lan>
2026-02-13 22:38:44 -0700

    Fix week 4 show items

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,14 +1,13 @@
 import ItemList from "./item-list";
+import items from "./items.json";
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-black text-white p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-     Shopping List
-      </h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Shopping List</h1>
 
       <div className="max-w-md mx-auto">
-        <ItemList />
+        <ItemList items={items} />
       </div>
     </main>
   );
