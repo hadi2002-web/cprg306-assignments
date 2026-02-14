1e85e1e9189cdfa44a87daaa921893e3503f05d0
Firdows Hadi <firdowshadi@Mac.lan>
2026-02-13 22:32:12 -0700

    Fix week 4 items map

--- app/week-4/item-list.js
+++ app/week-4/item-list.js
@@ -1,6 +1,6 @@
 import Item from "./item";
 
-export default function ItemList({ items }) {
+export default function ItemList({ items = [] }) {
   return (
     <ul className="space-y-3">
       {items.map((item) => (
