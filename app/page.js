c02a4e188b51eca63382510d09177d6d8567ae89
Firdows Hadi <firdowshadi@Mac.lan>
2026-02-23 23:00:37 -0700

    Week 6 complete - shopping list with sorting and styling

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function Page() {
         <Link href="/week-3">go to Week 3 {"-->"}</Link>
         <Link href="/week-4">go to Week 4 {"-->"}</Link>
         <Link href="/week-5">go to Week 5 {"-->"}</Link>
+        <Link href="/week-6">go to Week 6 {"-->"}</Link>
+  
 
       </p>
     </main>
---
+++ app/week-6/page.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import itemsData from "./items.json";
+import ItemList from "./ItemList";
+import NewItem from "./NewItem";
+
+export default function Page() {
+
+  const [items, setItems] = useState(itemsData);
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+   
+  return (
+  <main className="min-h-screen bg-black-700 flex justify-center items-start pt-10">
+    
+    <div className="bg-gray-800 w-full max-w-2xl p-8 rounded shadow-lg text-white">
+
+      <h1 className="text-3xl font-bold mb-6 border-b pb-2">
+        Shopping List
+      </h1>
+
+      <NewItem onAddItem={handleAddItem} />
+
+      <ItemList items={items} />
+
+    </div>
+
+  </main>
+);
+}
